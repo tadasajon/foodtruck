3948164267e6176c2259da3712cde3effe60c911
tadasajon <tadasajon@M1Air>
2022-05-04 17:44:21 -0400

    small adjustment so that the basic test passes

--- test/foodtruck_web/controllers/page_controller_test.exs
+++ test/foodtruck_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule FoodtruckWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "TRUCK"
+    assert html_response(conn, 200) =~ "MENU"
+    assert html_response(conn, 200) =~ "ADDRESS"
   end
 end
